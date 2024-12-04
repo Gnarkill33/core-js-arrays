2d79859fd42a9c653742e60420ba2b2699cc9a76
Gnarkill33 <gnarkill33@gmail.com>
2024-12-05 00:17:05 +0300

    feat: finish part 6

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -577,9 +577,9 @@ function shiftArray(arr, n) {
     return lastPiece.concat(firstPiece);
   }
   const nDifferent = n + arrayLength;
-  const firstPart = arr.slice(0, nDifferent);
-  const lastPart = arr.slice(nDifferent);
-  return firstPart.concat(lastPart);
+  const firstPart = arr.slice(0, -nDifferent);
+  const lastPart = arr.slice(-nDifferent);
+  return lastPart.concat(firstPart);
 }
 
 /**
@@ -595,8 +595,21 @@ function shiftArray(arr, n) {
  *   sortDigitNamesByNumericOrder([ 'nine','eight','nine','eight' ]) => [ 'eight','eight','nine','nine']
  *   sortDigitNamesByNumericOrder([ 'one','one','one','zero' ]) => [ 'zero','one','one','one' ]
  */
-function sortDigitNamesByNumericOrder(/* arr */) {
-  throw new Error('Not implemented');
+function sortDigitNamesByNumericOrder(arr) {
+  const numbers = {
+    zero: 0,
+    one: 1,
+    two: 2,
+    three: 3,
+    four: 4,
+    five: 5,
+    six: 6,
+    seven: 7,
+    eight: 8,
+    nine: 9,
+  };
+  const arraySorted = arr.sort((a, b) => numbers[a] - numbers[b]);
+  return arraySorted;
 }
 
 /**
@@ -618,8 +631,19 @@ function sortDigitNamesByNumericOrder(/* arr */) {
  *   swapHeadAndTail([]) => []
  *
  */
-function swapHeadAndTail(/* arr */) {
-  throw new Error('Not implemented');
+function swapHeadAndTail(arr) {
+  if (arr.length <= 1) return arr;
+  if (arr.length % 2 === 0) {
+    const partOne = arr.slice(0, arr.length / 2);
+    const partTwo = arr.slice(arr.length / 2);
+    return partTwo.concat(partOne);
+  }
+
+  const pieceOne = arr.slice(0, Math.trunc(arr.length / 2));
+  const middleIndex = Math.trunc(arr.length / 2);
+  const middlePiece = arr.slice(middleIndex, -middleIndex);
+  const pieceTwo = arr.slice(-Math.trunc(arr.length / 2));
+  return pieceTwo.concat(middlePiece).concat(pieceOne);
 }
 
 module.exports = {
